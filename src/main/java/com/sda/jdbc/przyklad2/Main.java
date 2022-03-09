ce0423610db8fc01c6eaca082912449bfe4f99b1
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-09 19:31:44 +0100

    util

---
+++ src/main/java/com/sda/jdbc/przyklad2/Main.java
@@ -0,0 +1,9 @@
+package com.sda.jdbc.przyklad2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/sda/jdbc/util/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package com.sda.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private final String url = "jdbc:mysql://localhost:3306/shop?serverTime=CET";
+    private final String user = "root";
+    private final String password = "root";
+
+    private Connection connection;
+
+    public DatabaseConnection() {
+        try {
+            this.connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
