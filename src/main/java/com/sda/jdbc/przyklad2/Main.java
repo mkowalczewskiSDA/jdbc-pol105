40aee8cb761fe0a578dedd2ba63bcc62dec0cb38
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-09 19:52:15 +0100

    przyklad 2

--- src/main/java/com/sda/jdbc/przyklad2/Main.java
+++ src/main/java/com/sda/jdbc/przyklad2/Main.java
@@ -11,26 +11,21 @@ public class Main {
     private static int CITY_INDEX = 1;
 
     public static void main(String[] args) {
-
         DatabaseConnection databaseConnection = new DatabaseConnection();
-        String query = "Select * from Address where add_city = ?";
-
-
+        String parameter = "1=1";
+        String query = "Select * from Address where add_id = ?";
         try(PreparedStatement preparedStatement = databaseConnection
                             .getConnection()
                             .prepareStatement(query)
         ) {
-
-            preparedStatement.setString(CITY_INDEX, "Bydgoszcz");
+            preparedStatement.setString(CITY_INDEX, parameter);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("ADD_STREET"));
             }
-
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
     }
 
 }
