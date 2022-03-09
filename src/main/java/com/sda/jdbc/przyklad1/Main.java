0a49bbc479b3ddd5cbb9012c0453977841f730d8
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-09 18:54:46 +0100

    przyklad 1

--- src/main/java/com/sda/jdbc/przyklad1/Main.java
+++ src/main/java/com/sda/jdbc/przyklad1/Main.java
@@ -1,5 +1,38 @@
 package com.sda.jdbc.przyklad1;
 
+import java.sql.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/shop?serverTime=CET";
+        String user = "root";
+        String password = "root";
+
+        String query = "Select * from Country";
+
+
+        try(
+                Connection connection = DriverManager.getConnection(url, user, password);
+                Statement statement = connection.createStatement()
+        ) {
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+
+            while (resultSet.next()) {
+                System.out.println("Country ID "+ resultSet.getInt("CO_ID"));
+                System.out.println("Country Name "+ resultSet.getString("CO_NAME"));
+                System.out.println("Country Alias "+ resultSet.getString("CO_ALIAS"));
+            }
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 
 }
