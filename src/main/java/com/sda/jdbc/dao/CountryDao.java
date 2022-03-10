b71c2ee6b7128bd873804bbff71b52e090c942d8
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-10 17:42:36 +0100

    zadanie3

---
+++ src/main/java/com/sda/jdbc/dao/CountryDao.java
@@ -0,0 +1,11 @@
+package com.sda.jdbc.dao;
+
+import com.sda.jdbc.przyklad4.Country;
+
+import java.util.List;
+
+public interface CountryDao {
+
+    List<Country> getAll();
+
+}
---
+++ src/main/java/com/sda/jdbc/dao/CountryDaoImpl.java
@@ -0,0 +1,4 @@
+package com.sda.jdbc.dao;
+
+public class CountryDaoImpl implements CountryDao {
+}
